fc6a0b5983a4120e1f29bd18382e8741ebc02d8f
CodeforNik <nikhilnandanwar1@gmail.com>
2023-12-14 19:58:32 +0530

    working application

---
+++ Student Management Application/BackEnd code/StudentManagementApp/src/main/java/com/studentmanagement/beans/Student.java
@@ -0,0 +1,82 @@
+package com.studentmanagement.beans;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.NaturalId;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String firstName;
+	private String lastName;
+	
+	//Unique key
+	@NaturalId(mutable = true)
+	private String email;
+	private String department;
+	
+	public Student() {
+		super();
+	}
+	public Student(int id, String firstName, String lastName, String email, String department) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.department = department;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", department=" + department + "]";
+	}
+
+}
---
+++ Student Management Application/BackEnd code/StudentManagementApp/src/main/java/com/studentmanagement/service/StudentServiceImpl.java
@@ -0,0 +1,68 @@
+package com.studentmanagement.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.studentmanagement.beans.Student;
+import com.studentmanagement.exception.StudentAlreadyExistsException;
+import com.studentmanagement.exception.StudentNotFoundException;
+import com.studentmanagement.respository.StudentRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class StudentServiceImpl implements StudentService{
+	
+	@Autowired
+	private StudentRepository studentRepository;
+	@Override
+	public Student addStudent(Student student) {
+		if(studentAlreadyExists(student.getEmail())) {
+			throw new StudentAlreadyExistsException(student.getEmail()+ "Already Exists");
+		}
+
+		return studentRepository.save(student);
+	}
+
+	@Override
+	public List<Student> getStudents() {
+		// TODO Auto-generated method stub
+		return studentRepository.findAll();
+	}
+
+	@Override
+	public Student updateStudent(Student student, int id) {
+		// TODO Auto-generated method stub
+		return studentRepository.findById(id).map(st -> {
+			st.setFirstName(student.getFirstName());
+			st.setLastName(student.getLastName());
+			st.setEmail(student.getEmail());
+			st.setDepartment(student.getDepartment());
+			
+			return studentRepository.save(st);
+		}).orElseThrow(()-> new StudentNotFoundException("Sorry, this student could not be found"));
+	}
+
+	@Override
+	public Student getStudentById(int id) {
+		// TODO Auto-generated method stub
+		return studentRepository.findById(id).orElseThrow(() -> new StudentNotFoundException("Sorry, no student found with the Id: " +id));
+	}
+
+	@Override
+	public void deleteStudent(int id) {
+		if(!studentRepository.existsById(id)) {
+			throw new StudentNotFoundException("Sorry, Student not found");
+		}
+		studentRepository.deleteById(id);
+	}
+	
+	private boolean studentAlreadyExists(String email) {
+		// TODO Auto-generated method stub
+		return studentRepository.findByEmail(email).isPresent();
+	}
+
+}
